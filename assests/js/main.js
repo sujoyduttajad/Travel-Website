8e4836ffc764d15714c78befa0eb4480b443ed95
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2021-07-24 17:21:48 +0530

    Changed background colors

--- assests/js/main.js
+++ assests/js/main.js
@@ -1,44 +1,44 @@
-const navMenu = document.getElementById('nav-menu');
-const navToggle = document.getElementById('nav-toggle');
-const navClose = document.getElementById('nav-close');
+/*==================== SHOW MENU ====================*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
 
-// ############     MENU Show     ###############
-if(navToggle) {
-    navToggle.addEventListener('click', () => {
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
         navMenu.classList.add('show-menu')
     })
 }
 
-// ############     Menu Hidden    ###############
-if(navClose) {
-    navClose.addEventListener('click', () => {
-        navMenu.classList.remove('show-menu');
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
     })
 }
 
-// ############     REMOVE MENU MOBILE     ###############
-const navLink = document.querySelector('.nav__link');
+/*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll('.nav__link')
 
-function linkAction() {
-    const navMenu = document.getElementById('nav-menu');
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
     // When we click on each nav__link, we remove the show-menu class
     navMenu.classList.remove('show-menu')
 }
-navLink.forEach(n => n.addEventListener('click', linkAction));
+navLink.forEach(n => n.addEventListener('click', linkAction))
 
-// ############     CHANGE BACKGROUND HEADER     ###############
-function scrollHeader() {
-    const header = document.getElementById('header');
+
+/*==================== CHANGE BACKGROUND HEADER ====================*/
+function scrollHeader(){
+    const header = document.getElementById('header')
     // When the scroll is greater than 100 viewport height, add the scroll-header class to the header tag
-    if(this.scrollY >= 100) {
-        header.classList.add('scroll-header');
-    } else {
-        header.classList.remove('scroll-header')
-    }
+    if(this.scrollY >= 100) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
 }
-window.addEventListener('scroll', scrollHeader);
+window.addEventListener('scroll', scrollHeader)
 
-// ############     SWIPER DISCOVER     ###############
+/*==================== SWIPER DISCOVER ====================*/
 let swiper = new Swiper(".discover__container", {
     effect: "coverflow",
     grabCursor: true,
@@ -172,4 +172,3 @@ themeButton.addEventListener('click', () => {
     localStorage.setItem('selected-theme', getCurrentTheme())
     localStorage.setItem('selected-icon', getCurrentIcon())
 })
-
