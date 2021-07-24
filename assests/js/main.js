bb9b2c84bd17b3bb95b7f383dd4421a991b1e4af
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2021-07-24 14:29:05 +0530

    code to remove sidebar menu

--- assests/js/main.js
+++ assests/js/main.js
@@ -8,3 +8,20 @@ if(navToggle) {
         navMenu.classList.add('show-menu')
     })
 }
+
+// ############     Menu Hidden    ###############
+if(navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu');
+    })
+}
+
+// ############     REMOVE MENU MOBILE     ###############
+const navLink = document.querySelector('.nav__link');
+
+function linkAction() {
+    const navMenu = document.getElementById('nav-menu');
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction));
